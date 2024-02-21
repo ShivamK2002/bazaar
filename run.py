b61199030957275b221593fc8f58d97ae08165ff
Shivam Kawale <ultimateengineer2002@gmail.com>
2024-02-21 18:29:18 +0530

    add gunicorn

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from market import app
+from market import create_app
 
-#Checks if the run.py file has executed directly and not imported
+app = create_app()
+
+# Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
     app.run(debug=True)
